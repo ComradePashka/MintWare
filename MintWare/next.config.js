976b9d090251603a216490b8873a2d8f70fa2cf0
Pavel <comradepashka@gmail.com>
2024-09-26 20:04:14 +0700

    dynamic buildId at next.config.js

--- MintWare/next.config.js
+++ MintWare/next.config.js
@@ -20,6 +20,15 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+
+  distDir: "_next",
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
+  },  
 };
 
 const plugins = [
