e912415af4489716a4f999deefa99dad72c5f90c
Pavel <comradepashka@gmail.com>
2024-09-30 21:05:49 +0700

    minor fixes + new nx config 4 prod deployment

--- MintWare/components/jupiter/terminal.tsx
+++ MintWare/components/jupiter/terminal.tsx
@@ -7,8 +7,6 @@ import { useWallet } from '@solana/wallet-adapter-react'; // Or @jup-ag/wallet-a
 import { init, syncProps } from '@jup-ag/terminal';
 import "@jup-ag/terminal/css";
 
-//import { Terminal } from '@jupyterlab/services';  // Example terminal service
-
 const JupiterTerminalWidget = () => {
     const walletContextState = useWallet()
 
@@ -23,25 +21,6 @@ const JupiterTerminalWidget = () => {
     useEffect(() => {
         syncProps({ passthroughWalletContextState: walletContextState })
     }, [walletContextState]);
-
-
-    // const terminalRef = useRef(null);
-
-    // useEffect(() => {
-    //     const startTerminal = async () => {
-    //     const session = await Terminal.startNew(); // Starting Jupyter terminal
-    //     const widget = new Terminal(session);      // Create terminal widget
-
-    //     // Attach the widget to the DOM
-    //     terminalRef.current.appendChild(widget.node);
-
-    //     return () => {
-    //         widget.dispose();
-    //         session.shutdown();
-    //     };
-    //     };
-    //     startTerminal();
-    // }, []);
 
     return <div id="integrated-terminal"></div>;
 };
