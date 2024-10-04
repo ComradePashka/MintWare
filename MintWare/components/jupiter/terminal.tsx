8fab640df41d3a5d6a4ecb839a258d3f20141d79
Pavel <comradepashka@gmail.com>
2024-10-04 13:24:10 +0700

    Jupiter widget fix, Prisma ORM + DB, Project list

--- MintWare/components/jupiter/terminal.tsx
+++ MintWare/components/jupiter/terminal.tsx
@@ -4,13 +4,16 @@ import React, { useEffect } from 'react';
 
 import { useWallet } from '@solana/wallet-adapter-react'; // Or @jup-ag/wallet-adapter;
 
+import './../../app/global.css';
+
+
 const JupiterTerminalWidget = () => {
     const walletContextState = useWallet()
 
     useEffect(() => {
         window.Jupiter.init({ 
             displayMode: 'integrated',
-            integratedTargetId: 'integrated-terminal',
+            integratedTargetId: 'jupiter',
             endpoint: 'https://api.mainnet-beta.solana.com',
             enableWalletPassthrough: true 
         });
@@ -27,7 +30,7 @@ const JupiterTerminalWidget = () => {
 //        syncProps({ passthroughWalletContextState: walletContextState })
     }, [walletContextState.connected]);
 
-    return <div id="integrated-terminal"></div>;
+    return <div id="jupiter" className='jupiter'></div>;
 };
 
 export default JupiterTerminalWidget;
