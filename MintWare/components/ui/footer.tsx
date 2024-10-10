4315547ac62af744c4ba755891122b26cab55f90
Zero!!! Dev <zerodev@Zeros-Mac-Studio.local>
2024-10-10 20:38:27 +0700

    update footer ui

--- MintWare/components/ui/footer.tsx
+++ MintWare/components/ui/footer.tsx
@@ -1,15 +1,19 @@
 import { ReactElement } from 'react';
 
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default function Footer(): ReactElement {
   return (
-    <footer className="flex h-[772px] w-full border-t bg-[url('/assets/gradient-bg.png')] bg-cover bg-center p-10">
-      <div className="flex flex-col h-[390px] w-full bg-white rounded rounded-tl-[16px] rounded-tr-[40px] rounded-bl-[40px] rounded-br-[16px]">
-        <div className="flex flex-row w-full p-10">
+    <footer className="flex flex-col min-h-[772px] w-full border-t bg-[url('/assets/gradient-bg.png')] bg-cover bg-center p-10">
+      <div className="flex flex-col min-h-[390px] w-full bg-white rounded rounded-tl-[16px] rounded-tr-[40px] rounded-bl-[40px] rounded-br-[16px] p-10">
+        <div className="flex flex-row w-full">
           <div className="flex flex-col flex-1">
-            <img
-              className="h-[48px] w-[160px]"
+            <Image
               alt="Logo"
               src="/assets/logo-group.png"
+              width={160}
+              height={48}
             />
           </div>
           <div className="flex flex-row flex-1 justify-between">
@@ -86,23 +90,63 @@ export default function Footer(): ReactElement {
             </p>
           </div>
         </div>
-        <div className="flex flex-row w-full pl-10 pr-10 justify-between">
+        <div className="flex flex-row w-full justify-between mt-auto">
           <div className="flex flex-row flex-1">
-            <div className="flex flex-row w-[40px] h-[40px] items-center justify-center rounded rounded-full bg-[#292626]">
-              <svg
-                className="text-[#292626]"
-                width="28"
-                height="28"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                fill="white"
-                viewBox="0 0 23 23"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4" />
-              </svg>
-            </div>
+            <Link
+              href="#"
+              className="flex flex-row w-[40px] h-[40px] items-center justify-center rounded rounded-full bg-[#292626]"
+            >
+              <Image
+                alt="Telegram Logo"
+                src="/assets/telegram.png"
+                width={18}
+                height={14}
+              />
+            </Link>
+            <Link
+              href="#"
+              className="flex flex-row w-[40px] h-[40px] items-center justify-center rounded rounded-full bg-[#292626] ml-3"
+            >
+              <Image
+                alt="Telegram Logo"
+                src="/assets/discord.png"
+                width={18}
+                height={14}
+              />
+            </Link>
+            <Link
+              href="#"
+              className="flex flex-row w-[40px] h-[40px] items-center justify-center rounded rounded-full bg-[#292626] ml-3"
+            >
+              <Image
+                alt="Telegram Logo"
+                src="/assets/x.png"
+                width={18}
+                height={14}
+              />
+            </Link>
+            <Link
+              href="#"
+              className="flex flex-row w-[40px] h-[40px] items-center justify-center rounded rounded-full bg-[#292626] ml-3"
+            >
+              <Image
+                alt="Telegram Logo"
+                src="/assets/medium.png"
+                width={18}
+                height={14}
+              />
+            </Link>
+            <Link
+              href="#"
+              className="flex flex-row w-[40px] h-[40px] items-center justify-center rounded rounded-full bg-[#292626] ml-3"
+            >
+              <Image
+                alt="Telegram Logo"
+                src="/assets/whatsapp.png"
+                width={18}
+                height={14}
+              />
+            </Link>
           </div>
           <div className="flex flex-col flex-1 justify-end">
             <p className="text-xs font-normal text-right text-[#292626]">
@@ -113,7 +157,57 @@ export default function Footer(): ReactElement {
             </p>
           </div>
         </div>
-        <br />
+      </div>
+      <div className="flex flex-row w-full justify-between mt-[48px]">
+        <div className="flex flex-col flex-1">
+          <p className="text-3xl font-normal text-white">
+            Never want to miss a sale?
+          </p>
+          <p className="text-6xl font-bold text-white mt-[8px]">
+            Sign up for our newsletter <br /> and get the latest news and <br />
+            updates.
+          </p>
+        </div>
+        <div className="flex flex-col flex-1 items-end justify-end">
+          <p className="text-xl font-normal text-white">
+            Send us your contact email and we will contact you.
+          </p>
+          <Link
+            href="#"
+            className="flex flex-row w-[519px] h-[59px] items-start justify-center full-width text-white border-[1px] border-white px-6 py-2 rounded rounded-lg mt-4"
+          >
+            <div className="flex flex-col flex-1">
+              <text className="text-sm font-normal text-white text-opacity-50">
+                Your email
+              </text>
+              <text className="text-xl font-bold text-white">
+                someone@mail.com
+              </text>
+            </div>
+            <div className="flex flex-col flex-1 h-full items-end justify-center">
+              <Image
+                alt="arrow"
+                src="/assets/arrow-next.png"
+                width={24}
+                height={24}
+              />
+            </div>
+          </Link>
+        </div>
+      </div>
+      <br />
+      <br />
+      <div className="flex flex-row w-full justify-between">
+        <div className="flex flex-col flex-1">
+          <p className="text-md font-thin text-white">
+            © Copyright MintWare 2024. All rights reserved.
+          </p>
+        </div>
+        <div className="flex flex-col flex-1 items-end justify-end">
+          <p className="text-md font-thin text-white">
+            Privacy Policy | Terms and Conditions
+          </p>
+        </div>
       </div>
     </footer>
   );
