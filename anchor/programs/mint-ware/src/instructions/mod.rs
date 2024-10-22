f366e740f6bba1d7e3375884a25e66301f84f9dc
Pavel Sokolov <ComradePashka@gmail.com>
2024-10-23 06:58:22 +0700

    project create update

--- anchor/programs/mint-ware/src/instructions/mod.rs
+++ anchor/programs/mint-ware/src/instructions/mod.rs
@@ -1,6 +1,6 @@
 //! All instructions
 pub mod settings_init;
-pub mod project_init;
+pub mod project;
 
 pub use settings_init::*;
-pub use project_init::*;
+pub use project::*;
--- anchor/programs/mint-ware/src/instructions/project.rs
+++ anchor/programs/mint-ware/src/instructions/project.rs
@@ -1,21 +1,22 @@
 use crate::constants::{PROJECT_PDA_SEED, SETTINGS_PDA_SEED, POOL_PDA_SEED};
 
 use crate::state::project::Project;
-use crate::state::settings::Settings;
 use anchor_lang::prelude::*;
 use anchor_spl::token::{Mint, Token, TokenAccount, Transfer};
 
 
 pub fn project_init(ctx: Context<ProjectInit>, name: String, description: String, reward_percent: u8, amount: u64) -> Result<()> {
+    msg!("INSIDE!! n{} d{} r{} a{}", name, description, reward_percent, amount);
+    msg!("Signer: {}", ctx.accounts.signer.key);
 
     let project = &mut ctx.accounts.project;
     project.authority = ctx.accounts.signer.key();
-    project.name = name.clone();
+    project.name = name;
     project.description = description;
     project.token_mint = ctx.accounts.token_mint.key();
     project.reward_percent = reward_percent;
 
-    msg!("Initialized new Project '{}' with reward {}", name, reward_percent);
+    msg!("Project created succesfully: {:?}", project);
 
     let inner = vec![
         PROJECT_PDA_SEED,
@@ -41,31 +42,30 @@ pub fn project_init(ctx: Context<ProjectInit>, name: String, description: String
     Ok(())
 }
 
+pub fn project_close(_ctx: Context<ProjectClose>) -> Result<()> {
+    // let project = &mut ctx.accounts.project;
+    // msg!("Project deleted succesfully: {:?}", project);
+    
+    //TODO: add token withdraw and closing related ATA here!!!
+    Ok(())
+}
+
+
 #[derive(Accounts)]
 pub struct ProjectInit<'info> {
     #[account(
         init,
         payer = signer,
-        space = 1000, // 8 + ? //TODO: set size using https://docs.rs/anchor-lang/latest/anchor_lang/derive.InitSpace.html
-        seeds = [PROJECT_PDA_SEED, signer.key().as_ref(), token_mint.key().as_ref()],
+        space = 8 + Project::INIT_SPACE, //accordin to: https://docs.rs/anchor-lang/latest/anchor_lang/derive.InitSpace.html
+        seeds = [PROJECT_PDA_SEED.as_ref(), signer.key().as_ref(), token_mint.key().as_ref()],
         bump,
     )]
     pub project: Account<'info, Project>,
 
-    //TODO: find global init or workaround with CPI?
     #[account(
         init_if_needed,
         payer = signer,
-        space = 56, // 8 + (8 + 8) . +32 Pubkey for reward_auth
-        seeds = [SETTINGS_PDA_SEED],
-        bump,
-    )]
-    pub settings: Account<'info, Settings>,
-
-    #[account(
-        init,
-        payer = signer,
-        seeds=[POOL_PDA_SEED, project.key().as_ref()],
+        seeds=[POOL_PDA_SEED.as_ref(), project.key().as_ref()],
         bump,
         token::mint=token_mint, //TODO: decide if ATA is really required?
         token::authority=project,
@@ -87,3 +87,27 @@ pub struct ProjectInit<'info> {
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
 }
+
+//TODO: find global init or workaround with CPI?
+// #[account(
+//     init_if_needed,
+//     payer = signer,
+//     space = 56, // 8 + (8 + 8) . +32 Pubkey for reward_auth
+//     seeds = [SETTINGS_PDA_SEED],
+//     bump,
+// )]
+// pub settings: Box<Account<'info, Settings>>,
+
+
+#[derive(Accounts)]
+pub struct ProjectClose<'info> {
+    #[account(
+        mut,
+        close = signer,
+    )]
+    pub project: Account<'info, Project>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
